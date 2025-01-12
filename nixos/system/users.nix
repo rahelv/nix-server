acc2c95aa2bfd734c2c5d9be9cd2e197c5123e29
Rahel <rahelkempf@gmx.ch>
2025-01-12 15:58:37 +0100

    update ssh keys

--- nixos/system/users.nix
+++ nixos/system/users.nix
@@ -13,9 +13,8 @@
       extraGroups = [ "networkmanager" "wheel" "docker" ];
       shell = pkgs.zsh;
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFz8ZLAhPmU5A/GoR9Td3IVeF0/LoVDyJSWiFAL0oAm4" #rahel
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1R2gEuXslK413gWBE4tOA894zO/MkhZrAK/LyRcsmo" #ephi
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3pquWdmxQFyxkNjBCX0HRfzWLUPh/LTYpA/Alc9OjB rahelkempf@gmx.ch"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFz8ZLAhPmU5A/GoR9Td3IVeF0/LoVDyJSWiFAL0oAm4" #rahel old 
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3pquWdmxQFyxkNjBCX0HRfzWLUPh/LTYpA/Alc9OjB" #rahel 
 
       ];
     };
