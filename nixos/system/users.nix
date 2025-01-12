c39d6544c498300cd748796ba9c49606d14fe5e2
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-01-12 10:19:47 +0100

    update ssh key

--- nixos/system/users.nix
+++ nixos/system/users.nix
@@ -15,6 +15,8 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFz8ZLAhPmU5A/GoR9Td3IVeF0/LoVDyJSWiFAL0oAm4" #rahel
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP1R2gEuXslK413gWBE4tOA894zO/MkhZrAK/LyRcsmo" #ephi
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3pquWdmxQFyxkNjBCX0HRfzWLUPh/LTYpA/Alc9OjB rahelkempf@gmx.ch"
+
       ];
     };
     siegi = {
